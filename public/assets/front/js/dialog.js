1a4258858edcdcc832612b18563c92922d736d42
TriosMerve <merve@trios.com.tr>
2025-09-10 11:33:55 +0300

    dialog icerik duzenlendi standartlastırıldı

---
+++ public/assets/front/js/dialog.js
@@ -0,0 +1,32 @@
+const allDialogs = document.querySelectorAll("dialog");
+
+// Kapatma için mevcut kod
+allDialogs.forEach((dialog) => {
+  dialog.addEventListener("click", (e) => {
+    const rect = dialog.getBoundingClientRect();
+    const isInDialog =
+      e.clientX >= rect.left &&
+      e.clientX <= rect.right &&
+      e.clientY >= rect.top &&
+      e.clientY <= rect.bottom;
+    if (!isInDialog) {
+      dialog.close();
+    }
+  });
+  const closeBtn = dialog.querySelector(".closeBtn");
+  if (closeBtn) {
+    closeBtn.addEventListener("click", () => dialog.close());
+  }
+});
+
+// Açma için ekleme
+document.querySelectorAll("[data-dialog]").forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const dialogId = btn.getAttribute("data-dialog");
+    const dialog = document.getElementById(dialogId);
+    if (dialog) {
+      dialog.showModal();
+    }
+  });
+});
