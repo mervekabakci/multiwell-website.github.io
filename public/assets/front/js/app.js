716485ff05698ea48635355c8d0ddbe06eeff1ba
TriosMerve <merve@trios.com.tr>
2025-09-15 12:50:01 +0300

    header fixed ozellıgı eklendı

--- public/assets/front/js/app.js
+++ public/assets/front/js/app.js
@@ -1,3 +1,17 @@
 window.onload = () => {
     document.querySelector(".preloader").remove();
 }
+
+
+function handleScroll() {
+  let scrollY = window.scrollY;
+
+  const header = document.querySelector("header");
+  if (header) {
+    const headerH = header.offsetHeight;
+    scrollY > headerH
+      ? header.classList.add("fixed")
+      : header.classList.remove("fixed");
+  }
+}
+window.addEventListener("scroll", handleScroll);
