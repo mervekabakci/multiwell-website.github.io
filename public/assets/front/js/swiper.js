9f1c51c623e84fdc1286e0704e58d665204677d7
TriosMerve <merve@trios.com.tr>
2025-09-16 11:52:20 +0300

    secondary buton hover ve footer portal guncellemesı yapıldı. slider oklar eklendı ve card hoverlar guncellendı

--- public/assets/front/js/swiper.js
+++ public/assets/front/js/swiper.js
@@ -1,16 +1,32 @@
-var swiper = new Swiper(".testingSlider", {
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+// testing sliderlar
+document.querySelectorAll(".testingWrapper").forEach(function (wrapper) {
+  new Swiper(wrapper.querySelector(".testingSlider"), {
+    spaceBetween: 30,
+    pagination: {
+      el: wrapper.querySelector(".swiper-pagination"),
+      clickable: true,
+    },
+    navigation: {
+      nextEl: wrapper.querySelector(".swiper-button-next"),
+      prevEl: wrapper.querySelector(".swiper-button-prev"),
+    },
+  });
 });
 
-var swiper = new Swiper(".commentsSlider", {
-  slidesPerView: 5,
-  spaceBetween: 60,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+
+
+// comments slider(lar)
+document.querySelectorAll(".commentsWrapper").forEach(function (wrapper) {
+  new Swiper(wrapper.querySelector(".commentsSlider"), {
+    slidesPerView: 5,
+    spaceBetween: 60,
+    pagination: {
+      el: wrapper.querySelector(".swiper-pagination"),
+      clickable: true,
+    },
+    navigation: {
+      nextEl: wrapper.querySelector(".swiper-button-next"),
+      prevEl: wrapper.querySelector(".swiper-button-prev"),
+    },
+  });
 });
