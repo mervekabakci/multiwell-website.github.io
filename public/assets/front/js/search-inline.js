5c4ab4ddb90a79f91dbe0f4ac56654a63089b36a
TriosMerve <merve@trios.com.tr>
2025-09-19 15:12:42 +0300

    inline search calısmasında dısarı tıklayınca arama alanını kapatma ve tekrar ınputa tıkladıgımız acılmasını sagladım

--- public/assets/front/js/search-inline.js
+++ public/assets/front/js/search-inline.js
@@ -1,4 +1,5 @@
 const searchWrapper = document.querySelector(".searchWrapperInline");
+const searchBody = searchWrapper.querySelector(".searchBody");
 const searchInput = searchWrapper.querySelector("input");
 const searchResult = searchWrapper.querySelector(".searchResult");
 
@@ -13,3 +14,15 @@ searchInput.addEventListener("input", () => {
     searchResult.style.display = "none";
   }
 });
+
+searchInput.addEventListener("focus", () => {
+    const value = searchInput.value.trim();
+    if(value.length >=3){
+        searchResult.style.display = "flex";
+    }
+})
+document.addEventListener("click", (e) => {
+    if(!searchBody.contains(e.target)){
+        searchResult.style.display = "none";
+    }
+})
