127d47ae00a0a1655d5c40e3ad30d6e90e161f56
TriosMerve <merve@trios.com.tr>
2025-09-24 13:41:28 +0300

    anasayfa ve mobi responsive duzenlemeler yapıldı

--- public/assets/front/js/swiper.js
+++ public/assets/front/js/swiper.js
@@ -1,3 +1,67 @@
+// Featured sliderlar
+document.querySelectorAll(".featuredWrapper").forEach(function (wrapper) {
+  new Swiper(wrapper.querySelector(".featuredSlider"), {
+    slidesPerView: 1.5,
+    spaceBetween: 16,
+    pagination: {
+      el: wrapper.querySelector(".swiper-pagination"),
+      clickable: true,
+    },
+    navigation: {
+      nextEl: wrapper.querySelector(".swiper-button-next"),
+      prevEl: wrapper.querySelector(".swiper-button-prev"),
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+      1440: {
+        slidesPerView: 5,
+        spaceBetween: 20,
+      },
+    },
+  });
+});
+
+// quad sliderlar
+document.querySelectorAll(".quadWrapper").forEach(function (wrapper) {
+  new Swiper(wrapper.querySelector(".quadSlider"), {
+    slidesPerView: 1,
+    spaceBetween: 16,
+    pagination: {
+      el: wrapper.querySelector(".swiper-pagination"),
+      clickable: true,
+    },
+    navigation: {
+      nextEl: wrapper.querySelector(".swiper-button-next"),
+      prevEl: wrapper.querySelector(".swiper-button-prev"),
+    },
+    breakpoints: {
+      576: {
+        slidesPerView: 2,
+        spaceBetween: 15,
+      },
+      768: {
+        slidesPerView: 3,
+        spaceBetween: 15,
+      },
+      1200: {
+        slidesPerView: 4,
+        spaceBetween: 60,
+      },
+    },
+  });
+});
+
 // testing sliderlar
 document.querySelectorAll(".testingWrapper").forEach(function (wrapper) {
   new Swiper(wrapper.querySelector(".testingSlider"), {
